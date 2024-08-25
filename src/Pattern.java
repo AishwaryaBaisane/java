5b79279c1f8baf67903c4e53c50a736f474903a1
baisaneaishwarya <baisanehira@gmail.com>
2024-08-25 09:26:42 +0530

    24-08-2024

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,20 +1,35 @@
-import java.util.Scanner;
 
 public class Pattern {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter N Number :");
-        int value = scanner.nextInt();
-
-
-        int x = 0;
-        for (int i = 0; i <= value; i++) {
-            if (i / 2 == 0) {
-                x = i;
-                x = x + x;
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 12; j++) {
+                if (i == 1 && j == 7) {
+                    System.out.print("* ");
+                } else if (i == 2 && (j == 6 || j == 7))
+                {
+                    System.out.print("* ");
+                } else if (i == 3 && (j == 5 || j == 8))
+                {
+                    System.out.print("* ");
+                } else if (i == 4 && (j == 4 || j == 9))
+                {
+                    System.out.print("* ");
+                } else if (i == 5 && (j == 3 || j == 10))
+                {
+                    System.out.print("* ");
+                } else if (i == 6 && (j == 2 || j == 11))
+                {
+                    System.out.print("* ");
+                } else if (i == 7 && (j == 1 || j == 12))
+                {
+                    System.out.print("* ");
+                } else if (i == 8) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
+            System.out.print("\n");
         }
-        System.out.println("Sum Is:" + x);
     }
 }
